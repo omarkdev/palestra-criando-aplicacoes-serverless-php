b6fe079c37c6a0d47bfc97a269d604ee5fe30fc1
Marcos Felipe <omarkdev@gmail.com>
2019-10-20 11:31:25 -0300

    Roteamento simples: Configurando e criando o roteamento com slim

--- simple-routing/index.php
+++ simple-routing/index.php
@@ -1,3 +1,22 @@
 <?php
 
-echo 'Hello world!';
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+$app->run();
